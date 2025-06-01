0ab9e7231bbb1091b1395fff0c067b277fdee401
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-06-01 18:56:22 -0400

    added drinking water model

---
+++ WaterLogger/Models/DrinkingWaterModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterLogger.Models
+{
+    public class DrinkingWaterModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- WaterLogger/obj/Debug/net9.0/WaterLogger.AssemblyInfo.cs
+++ WaterLogger/obj/Debug/net9.0/WaterLogger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ff4d5b9b853e54b6107093e0079ed1ec6cda8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f0ab41f2edaa81782cb11c958dbb5007c1bbce")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
