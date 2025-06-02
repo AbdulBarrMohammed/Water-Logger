1a63eda8623be54190e1f4e8d40a5b0ee7ad3799
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-06-02 13:48:40 -0400

    done

--- WaterLogger/Models/DrinkingWaterModel.cs
+++ WaterLogger/Models/DrinkingWaterModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,10 @@ namespace WaterLogger.Models
     public class DrinkingWaterModel
     {
         public int Id { get; set; }
+
         public DateTime Date { get; set; }
+
+        [Range(0, Int32.MaxValue, ErrorMessage = "Value for {0} must be positive.")]
         public int Quantity { get; set; }
     }
 }
