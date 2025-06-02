ff776b0c528995a8377f8dba011b3f18ddbbf45f
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-06-02 12:17:36 -0400

    added delete

--- WaterLogger/obj/Debug/net9.0/WaterLogger.AssemblyInfo.cs
+++ WaterLogger/obj/Debug/net9.0/WaterLogger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf27eccd2e60ef94ef78e70a4443a98657baafd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9b94696a9d0538c6e5a5f5f862318226844633")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
